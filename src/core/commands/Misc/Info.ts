0b68735d737f9c6f7f9461cd59b1a18aaa0b53ef
nerdthatnoonelikes <kenrag24@gmail.com>
2020-12-16 20:34:27 -0600

    add info command

---
+++ src/core/commands/Misc/Info.ts
@@ -0,0 +1,36 @@
+import { Command } from "discord-akairo";
+import { MessageEmbed, Message } from "discord.js";
+import os from "os";
+import dotenv from "dotenv";
+import fs from "fs";
+
+export default class InfoCommand extends Command {
+    public constructor() {
+        super("info", {
+            aliases: ["info", "information", "botinfo"],
+            description: {
+                content: "see some information about the bot"
+            },
+            category: "Misc"
+        });
+    }
+    
+    public async exec(message: Message) {
+        const msg = await message.util.send("Fetching info...");
+        const usedmemory = process.memoryUsage().heapUsed / 1024 / 1024
+        const usedmemoryinmb = `${Math.round(usedmemory)}`
+            
+        const fileStr = await fs.promises.readFile("/etc/os-release");
+        const data = dotenv.parse(fileStr);
+
+        const embed = new MessageEmbed()
+            .setTitle(`Info about ${this.client.user.username}`)
+            .setColor("RANDOM")
+            .addField("Memory Usage", `${usedmemoryinmb}MB`)
+            .addField("CPU Architecture", os.arch())
+            .addField("Operating System", `${os.platform()} | ${data.NAME}`)
+
+        msg.delete();
+        message.channel.send(embed);
+    }
+}
