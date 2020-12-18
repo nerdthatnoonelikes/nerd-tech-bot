0bbdec8ca729cb234027e885bf2145284e9c3d7f
Nathan <71985262+NathanCubing@users.noreply.github.com>
2020-12-18 13:11:37 -0500

    Update Mute.ts

--- src/core/commands/Mod/Mute.ts
+++ src/core/commands/Mod/Mute.ts
@@ -30,39 +30,9 @@ export default class MuteCommand extends Command {
     }
 
     public async exec(message: Message, {member, reason}: {member: GuildMember, reason: string}) {
-        try {
-
-            let channels = message.guild.channels.cache.filter(ch => ch.type !== 'category');
-            let role = message.guild.roles.cache.find((x) => x.name === "Muted");
-
-            if (!role) {
-                role = await message.guild.roles.create({
-                    data: {
-                        name: "Muted",
-                        color: "#505050",
-                    },
-                    reason: "Role needed for Mute System"
-                })
-            }
-
-            channels.forEach(channel => {
-                channel.updateOverwrite(role, {
-                    SEND_MESSAGES: false,
-                })
-            })
-
-            let MuteEmbed = new MessageEmbed()
-                .setThumbnail(member.user.displayAvatarURL())
-                .setTitle('User Was Muted!')
-                .addField('Who Was Muted', member.user.tag)
-                .addField('Muted By', message.author.tag)
-                .addField('Reason', reason)
-                .setColor("RANDOM")
-                .setTimestamp()
-
-            message.util.send(MuteEmbed)
-
-            member.roles.add(role);
+        try 
+            member.roles.add("789166221018529843");
+            message.util.send(`${member.user.username} was muted by **${message.author.username}** for **${reason}**`);
         } catch (e) {
             message.util.send(`There was an error while executing that command | ${e}`);
         };
